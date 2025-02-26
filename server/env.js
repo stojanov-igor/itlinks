47e00b5dc8a3d120dff0ee40cafa764aa675870c
Stojanov Igor <83087510+JosephKnecht-lab@users.noreply.github.com>
2025-02-26 15:32:23 +0100

    edit name

--- server/env.js
+++ server/env.js
@@ -28,8 +28,8 @@ if (process.argv.includes("--production")) {
 
 const spec = {
   PORT: num({ default: 3000 }),
-  SITE_NAME: str({ example: "Kutt", default: "Kutt" }),
-  DEFAULT_DOMAIN: str({ example: "kutt.it", default: "localhost:3000" }),
+  SITE_NAME: str({ example: "ItLinks.me", default: "ItLinks.me" }),
+  DEFAULT_DOMAIN: str({ example: "ItLinks.me", default: "localhost:3000" }),
   LINK_LENGTH: num({ default: 6 }),
   LINK_CUSTOM_ALPHABET: str({ default: "abcdefghkmnpqrstuvwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789" }),
   TRUST_PROXY: bool({ default: true }),
